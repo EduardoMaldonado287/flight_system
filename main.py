c3cf5c07a717641de14cda81451d8a4f5e2e3c86
EduardoMaldonado287 <a00832361@itesm.mx>
2023-12-07 21:02:13 +0100

    Add flight selector and add passenger features

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ class RouteContainer:
     def get_origin_by_destination(self, destination):
         return [route.origin.name for route in self.routes if route.destination.name == destination]
 
+
 class Flight:
     def __init__(self, route, departure_time, duration, status, date, code, price, available_seats):
         self.flight_id = str(uuid.uuid4()) 
@@ -74,7 +75,9 @@ class Flight:
         self.available_seats = available_seats
 
     def flight_info(self):
-        return(self.date, self.departure_time.get_time(), self.duration.get_time(), self.price)
+        fInfo = ("From: " + self.route.origin.name + " to: " + self.route.destination.name + " - departure time"
+                 + self.departure_time.get_time() + " - duration: " + self.duration.get_time() + " - price: " + str(self.price))
+        return fInfo
         
         
 class FlightManager:
@@ -84,9 +87,11 @@ class FlightManager:
     def add_flight(self, flight):
         self.flights.append(flight)
         
-    # def get_flights(self):
-    #     for flight in self.flights:
-    #         print(flight.flight_info())
+    def get_flight_by_id(self, fid):
+        for flight in self.flights:
+            if flight.flight_id == fid:
+                return flight
+        print("The flight does not exist")
         
     def get_flights_in_route_and_date(self, origin, destination, date):
         flights = []
@@ -96,7 +101,7 @@ class FlightManager:
         for i in range(len(self.flights)):
             if (self.flights[i].route.origin.name == origin and self.flights[i].route.destination.name == destination
                 and str(self.flights[i].date) == date):
-                flights.append(str(self.flights[i].price))
+                flights.append(self.flights[i])
                 
         if len(flights) == 0:
             print("No flights to display at that date")           
@@ -116,7 +121,7 @@ class FlightManager:
 class ClientContainer:
     def __init__(self):
         self.client_list = []
-        self.current_client = Client("me", "pass")
+        self.current_client = None
     def add_new_client(self, client):
         self.client_list.append(client)
     def login(self, email, password):
@@ -156,25 +161,28 @@ class Ticket:
         self.passengers = passengers
         self.booking_number = booking_number
         # clientContainer.current_client.add_ticket(self)
-   
+
+
+class ServiceContainer:
+    def __init__(self):
+        self.services = []
         
+    def add_service(self, service):
+        self.services.append(service)
+
+
 class Service:
     def __init__(self, name="Basic Service", description="Just the seat", price = 0):
         self.name = name
         self.description = description
         self.price = price
-   
+        serviceContainer.add_service(self)
+    #     self.add_to_service_container()
+    
+    # def add_to_service_container(self):
+    #     serviceContainer.add_service(self)
         
-class Passenger:
-    def __init__(self, first_name, surname = "", date_of_birth = "", country="", service_type=Service()):
-        self.first_name = first_name
-        self.surname = surname
-        self.date_of_birth = date_of_birth
-        self.country = country
-        self.service_type = service_type
-        # self.seat = seat
-
-
+        
 class EconomyClass(Service):
     def __init__(self, price, features):
         super().__init__('Economy Class', 'Basic services', price)
@@ -186,11 +194,22 @@ class BusinessClass(Service):
         super().__init__('Business Class', 'Enhanced services', price)
         self.features = features
         
-
+        
+class Passenger:
+    def __init__(self, first_name, surname = "", date_of_birth = "", country="", service_type=""):
+        self.first_name = first_name
+        self.surname = surname
+        self.date_of_birth = date_of_birth
+        self.country = country
+        self.service_type = service_type
+        # self.seat = seat
+        
+# BusinessClass(100)
 clientContainer = ClientContainer()
 flightManager = FlightManager()
 routeContainer = RouteContainer()
 cityContainer = CityContainer()
+serviceContainer = ServiceContainer()
 
 route1 = Route(City(), City())
 Route(City("Aarhus"), City("Billund"))
@@ -202,11 +221,14 @@ print(routeContainer.get_destinations_by_origin("Aarhus"))
 today = datetime.date.today()
 flightManager.create_flight_schedule(route1, Time(1, 2), Time(1, 3), 1, "232", 123, 200, today, 30, [0,1, 2, 3, 4, 5, 6])
 
+flightManager.create_flight_schedule(route1, Time(14, 20), Time(1, 0), 1, "232", 123, 200, today, 30, [0,1, 2, 3, 4, 5, 6])
+
 
 class FlightSelection:
     def __init__(self, root):
         self.root = root
-
+        self.create_widget()
+        
     def create_widget(self):
         # Combobox para mostrar rutas existentes
         self.departure_combobox = ttk.Combobox(self.root, state="readonly")
@@ -242,30 +264,34 @@ class FlightSelection:
 class FlightDisplay:
     def __init__(self, root, x_coord, y_coord):
         self.root = root
-
+        self.flights = ""
         # Lista de strings de ejemplo
         self.string_list = []
 
         # Crear el marco principal
         self.main_frame = ttk.Frame(root)
-        self.main_frame.place(x=x_coord, y=y_coord, height=300, width=500)
+        self.main_frame.place(x=x_coord, y=y_coord, height=300, width=650)
 
-    def create_display(self, values):
-
+    def show_available_flights(self, flights):
+        self.flights = flights
         # Crear un Listbox para mostrar las strings
         self.listbox = tk.Listbox(self.main_frame, selectmode=tk.SINGLE, bg="white", font=("Arial", 12))
         self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=(0, 10), pady=(0, 5))
-
-        self.string_list = values
-        # Agregar las strings al Listbox con un espacio en blanco al final
-        for string in self.string_list:
-            self.listbox.insert(tk.END, string + "\n" + ".")
-
         # Crear una barra de desplazamiento
         scrollbar = ttk.Scrollbar(self.main_frame, orient=tk.VERTICAL, command=self.listbox.yview)
-        scrollbar.place(x=465, y=5, height=290)
-
+        scrollbar.place(x=615, y=5, height=290)
         self.prev_selected_index = None
+        
+        self.string_list = []
+        
+        for i in range (len(flights)):
+            self.string_list.append(flights[i].flight_info())
+            
+        # Agregar las strings al Listbox con un espacio en blanco al final
+        for string in self.string_list:
+            self.listbox.insert(tk.END, string)
+
+
 
         # Asociar la función de clic al evento <<ListboxSelect>>
         self.listbox.bind("<<ListboxSelect>>", self.on_item_select)
@@ -274,6 +300,7 @@ class FlightDisplay:
     def on_item_select(self, event):
         # Obtener el índice del elemento seleccionado
         selected_index = self.listbox.curselection()
+        # print(selected_index[0])
 
         if selected_index:
             # Restaurar el color original de la cadena seleccionada anteriormente
@@ -290,11 +317,17 @@ class FlightDisplay:
             selected_value = self.string_list[selected_index[0]]
             print(f"Valor seleccionado: {selected_value}")
 
-    def add_items(self, lista):
-        self.string_list.extend(lista)
+
+    def get_selected_flight(self):
+        if (self.listbox.curselection()):
+            index = self.listbox.curselection()[0]
+            return flightManager.get_flight_by_id(self.flights[index].flight_id)
+        print("No hay ningun vuelo seleccionado")
+        return False
         
-    def get_flights(self, flights):
-        pass
+    def hide(self):
+        self.main_frame.place_forget()
+
 
 class Calendar:
     def __init__(self, root):
@@ -341,43 +374,206 @@ class Calendar:
         if self.fdate == "":
             return False
         return self.fdate
+
+
+class PassengerForm:
+    SERVICE_OPTIONS = ["Service", "Premium Service", "Super Service"]
+
+    def __init__(self, root):
+        # self.add_passenger_callback = add_passenger_callback
+        self.root = root
+
+        self.main_frame = ttk.Frame(root)
+        
+        self.passenger_list = []
+        self.first_name_var = tk.StringVar()
+        self.surname_var = tk.StringVar()
+        self.date_of_birth_var = tk.StringVar()
+        self.country_var = tk.StringVar()
+        self.service_type_var = tk.StringVar()
+
+        # self.add_labels()
+        
+    def add_labels(self):
+        # Etiquetas y campos de entrada
+        self.main_frame.place(x=100, y=200, height=300, width=650)
+
+        ttk.Label(self.main_frame, text="First Name:").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.first_name_var).grid(row=0, column=1, padx=5, pady=5)
+
+        ttk.Label(self.main_frame, text="Surname:").grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.surname_var).grid(row=1, column=1, padx=5, pady=5)
+
+        ttk.Label(self.main_frame, text="Date of Birth:").grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.date_of_birth_var).grid(row=2, column=1, padx=5, pady=5)
+
+        ttk.Label(self.main_frame, text="Country:").grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.country_var).grid(row=3, column=1, padx=5, pady=5)
+
+        ttk.Label(self.main_frame, text="Service Type:").grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
+        
+        # Dropdown menu for Service Type
+        self.service_type_menu = ttk.Combobox(self.main_frame, textvariable=self.service_type_var, values=self.SERVICE_OPTIONS, state="readonly")
+        self.service_type_menu.grid(row=4, column=1, padx=5, pady=5)
+
+        # Botón para guardar la información del pasajero
+        self.save_passenger_button = ttk.Button(self.root, text="Add Passenger 1", command=self.save_passenger)
+        self.save_passenger_button.place(x=100, y= 450)
+
+        # Contador de pasajeros
+        self.passenger_count = 1
+        
+    def save_passenger(self):
+        # Crear un objeto Passenger con la información ingresada
+        
+        if (not self.first_name_var.get() or not self.surname_var.get() or not self.date_of_birth_var.get()
+            or not self.country_var.get() or not self.service_type_var.get()):
+            print("Tiene que completar todos los campos")
+            return
+        
+        self.view_order_and_pay_btn.place(x= 225, y= 450)
+
+        passenger = Passenger(
+            first_name=self.first_name_var.get(),
+            surname=self.surname_var.get(),
+            date_of_birth=self.date_of_birth_var.get(),
+            country=self.country_var.get(),
+            # buscar objeto de service type
+            service_type=self.service_type_var.get()
+        )
+
+        self.passenger_list.append(passenger)
+        
+        self.first_name_var.set("")
+        self.surname_var.set("")
+        self.date_of_birth_var.set("") 
+        self.country_var.set("")
+        self.service_type_var.set("") 
+        self.passenger_count += 1
+
+        # Actualizar el texto del botón con el número actual de pasajeros
+        self.save_passenger_button["text"] = f"Add Passenger {self.passenger_count}"
+        # view order and pay
+    def dissapear(self):
+        self.place_forget()
+        self.pack_forget()
+        
+    def get_passenger_list(self):
+        if len(self.passenger_list) == 0:
+            return False
+        return self.passenger_list
             
+    
+class ClientLogin:
+    def __init__(self, root):
+        self.main_frame = ttk.Frame(root)
+        self.main_frame.place(x=100, y=100, height=300, width=300)
+
+        self.root = root
+        self.root.title("Client Login")
+
+        self.client_container = clientContainer
+
+        # Variables de control para los campos de entrada
+        self.email_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+
+        # Etiquetas y campos de entrada
+        ttk.Label(self.main_frame, text="Email:").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.email_var).grid(row=0, column=1, padx=5, pady=5)
+
+        ttk.Label(self.main_frame, text="Password:").grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.password_var, show="*").grid(row=1, column=1, padx=5, pady=5)
+
+        # Botones de Login y Register
+        ttk.Button(self.main_frame, text="Login", command=self.login).grid(row=2, column=0, columnspan=2, pady=10)
+        ttk.Button(self.main_frame, text="Register", command=self.register).grid(row=3, column=0, columnspan=2, pady=10)
+
+    def login(self):
+        email = self.email_var.get()
+        password = self.password_var.get()
+
+        if self.client_container.login(email, password):
+            # Aquí puedes realizar acciones adicionales después del login exitoso
+            print("Redirect to main application after successful login.")
+        else:
+            # Aquí puedes mostrar un mensaje de error al usuario
+            print("Login failed.")
+
+    def register(self):
+        email = self.email_var.get()
+        password = self.password_var.get()
+
+        new_client = Client(email, password)
+        self.client_container.add_new_client(new_client)
+    
+    
+class FlightSummary:
+     pass
+    
+    
 class FlightBookingApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Flight Booking App")
-        root.geometry("900x600")
+        self.selected_flight = ""
 
         # Crear y agregar widgets a la interfaz utilizando la clase CreateWidget
         self.route_widget = FlightSelection(root)
-        self.route_widget.create_widget()
-
         self.calendar = Calendar(self.root)
-        
-        search_btn = Button(self.root, text ="search", command = self.get_flight_params)
+        self.flight_display = FlightDisplay(self.root, 100, 150)
+        self.passenger_widget = PassengerForm(self.root)
+        search_btn = Button(self.root, text ="search", command = self.search_flight)
         search_btn.place(x=820,y=30)
         
-        self.display = FlightDisplay(self.root, 200, 200)
+        profile_btn = Button(self.root, text ="search", command = self.search_flight)
         
-        # display.add_items(["hola", "aidos"])
+        self.search_btn2 = tk.Button(self.root, text="Select Flight", command= self.select_flight)
         
+        # view order and pay
+        self.view_order_and_pay_btn = tk.Button(self.root, text="View order and pay", command= self.check_out)
+
+     
+    def check_out(self):
+        if not self.passenger_widget.get_passenger_list():
+            print("You need to add a passenger to buy a tikcet")
         
-    def get_flight_params(self):
+        if not clientContainer.current_client:
+            pass
+            #create account
+        
+    def search_flight(self):
+        self.calendar.toggle_calendar()
+
+        
         origin = self.route_widget.get_origin()
         destination = self.route_widget.get_destination()
         date = self.calendar.get_date()
         
         if (origin and destination and date):
-            print(origin, destination, date)
             flighs_list = flightManager.get_flights_in_route_and_date(origin, destination, date)
-            self.display.create_display(flighs_list)
+            self.flight_display.show_available_flights(flighs_list)
+            
+            self.search_btn2.place(x=100, y=500)
         else:
-            print("error")
+            print("error, tiene que completar todos los campos")
         
-# Función principal para ejecutar la aplicación
+    def select_flight(self):
+        self.selected_flight = self.flight_display.get_selected_flight()
+        
+        if not self.selected_flight:
+            print("no flight selected")
+            return
+        
+        self.search_btn2.place_forget()
+        self.flight_display.hide()
+        self.passenger_widget.add_labels()
+        
+        
+# Función principal para ejecutar la aplicacion
 def main():
     root = tk.Tk()
+    root.title("Flight Booking App")
+    root.geometry("900x600")
     app = FlightBookingApp(root)
     root.mainloop()
-
 main()
--- test.py
+++ test.py
@@ -1,73 +1,84 @@
 import tkinter as tk
 from tkinter import ttk
 
-class LabelDisplayApp:
-    def __init__(self, root, string_list):
+class ClientContainer:
+    def __init__(self):
+        self.client_list = []
+        self.current_client = None
+
+    def add_new_client(self, client):
+        self.client_list.append(client)
+        print("Registration successful for user:", client.email)
+
+    def login(self, email, password):
+        for i in range(len(self.client_list)):
+            if email == self.client_list[i].email and password == self.client_list[i].password:
+                print("Login successful user:", email)
+                self.current_client = self.client_list[i]
+                return True
+        print("Incorrect username or password")
+        return False
+
+    def logout(self):
+        self.current_client = None
+
+class Client:
+    def __init__(self, email, password):
+        self.client_id = email  # Utilizamos el email como ID para simplificar el ejemplo
+        self.email = email
+        self.password = password
+
+# Clase para la interfaz gráfica
+class ClientLoginApp:
+    def __init__(self, root):
+        self.main_frame = ttk.Frame(root)
+        self.main_frame.place(x=100, y=100, height=300, width=300)
+
         self.root = root
-        self.string_list = string_list
+        self.root.title("Client Login")
 
-        # Create a green container frame
-        container_frame = tk.Frame(root, background="green")
-        container_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        self.client_container = clientContainer
 
-        # Create a frame to hold the Label widgets
-        self.frame = ttk.Frame(container_frame)
-        self.frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        # Variables de control para los campos de entrada
+        self.email_var = tk.StringVar()
+        self.password_var = tk.StringVar()
 
-        # Create a scrollbar for the frame
-        scrollbar = ttk.Scrollbar(self.frame, orient=tk.VERTICAL)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        # Etiquetas y campos de entrada
+        ttk.Label(self.main_frame, text="Email:").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.email_var).grid(row=0, column=1, padx=5, pady=5)
 
-        # Create a Canvas to contain the Label widgets
-        self.canvas = tk.Canvas(self.frame, yscrollcommand=scrollbar.set)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        ttk.Label(self.main_frame, text="Password:").grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Entry(self.main_frame, textvariable=self.password_var, show="*").grid(row=1, column=1, padx=5, pady=5)
 
-        # Attach the scrollbar to the canvas
-        scrollbar.config(command=self.canvas.yview)
+        # Botones de Login y Register
+        ttk.Button(self.main_frame, text="Login", command=self.login).grid(row=2, column=0, columnspan=2, pady=10)
+        ttk.Button(self.main_frame, text="Register", command=self.register).grid(row=3, column=0, columnspan=2, pady=10)
 
-        # Create a frame to contain the Label widgets inside the canvas
-        self.label_frame = ttk.Frame(self.canvas)
+    def login(self):
+        email = self.email_var.get()
+        password = self.password_var.get()
 
-        # Add the frame to the canvas
-        self.canvas.create_window((0, 0), window=self.label_frame, anchor=tk.NW)
+        if self.client_container.login(email, password):
+            # Aquí puedes realizar acciones adicionales después del login exitoso
+            print("Redirect to main application after successful login.")
+        else:
+            # Aquí puedes mostrar un mensaje de error al usuario
+            print("Login failed.")
 
-        # Bind the canvas to configure the scroll region
-        self.canvas.bind("<Configure>", self.configure_scroll_region)
+    def register(self):
+        email = self.email_var.get()
+        password = self.password_var.get()
 
-        # Add Label widgets to the label frame based on the string list
-        self.label_widgets = []
-        for text_string in self.string_list:
-            label_widget = tk.Label(self.label_frame, text=text_string, wraplength=300)
-            label_widget.pack(pady=5, anchor=tk.W)
-            self.label_widgets.append(label_widget)
+        new_client = Client(email, password)
+        self.client_container.add_new_client(new_client)
 
-    def configure_scroll_region(self, event):
-        # Update the scroll region when the size of the canvas changes
-        self.label_frame.update_idletasks()
-        self.frame_width = self.frame.winfo_width()
-        self.frame_height = self.frame.winfo_height()
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))
+clientContainer = ClientContainer()
+
+def main():
+    root = tk.Tk()
+    root.geometry("900x600")
+    app = ClientLoginApp(root)
+    root.mainloop()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-
-    # Lista de strings de ejemplo
-    string_list = ["String 1", "String 2", "String 3", "String 4", "String 5", "String 6", "String 7",
-                   "String 8", "String 9", "String 10", "String 11", "String 12", "String 13", "String 14"]
-
-    app = LabelDisplayApp(root, string_list)
-    root.title("Label Display App")
-
-    # Set the size of the main window
-    window_width = 500
-    window_height = 300
-    root.geometry(f"{window_width}x{window_height}")
-
-    # Center the window on the screen
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    x_position = (screen_width - window_width) // 2
-    y_position = (screen_height - window_height) // 2
-    root.geometry(f"+{x_position}+{y_position}")
-
-    root.mainloop()
+    main()
